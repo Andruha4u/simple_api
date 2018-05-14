201d5697e28bdaa80cb56de5dc536bf3e6db4050
AndrewThe <fipsher@gmail.com>
2018-05-14 20:13:55 +0300

    Upgrade mongoose

--- api/index.js
+++ api/index.js
@@ -2,15 +2,20 @@
 var config = require('./config.js');
 var server = require('./server.js');
 var routes = require('./routes.js');
-
-var mongoose = require('mongoose').connect(config.db_connection.url +
-                                           config.db_connection.port +
-                                           config.db_connection.db).connection
-                                                                     .on('error', console.error.bind(console, 'connection error:'))
-                                                                     .once('open', function() {
-                                                                            console.log('connected to mongo!');
-                                                                        });
-
+const options = {
+    autoIndex: false, // Don't build indexes
+    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0
+  };
+const connectionString = config.db_connection.url + config.db_connection.port + config.db_connection.db
+var mongoose = require('mongoose').connect(connectionString, options).then(
+    () => { console.log('connected to mongo!'); },
+    err => console.error.bind(console, 'connection error:')
+  );
+ 
 var repositoryFactory = require('../lib/repositoryFactory.js')(config.entities_generation);
 var routeFactory = require('./routeDefinition/routeFactory.js')(repositoryFactory); 
 var repositoryExtentions = require('../lib/repositoryExtentions')();
